d544ae847cea02a4ad7c8e523d35314130df78f0
Alek Mitrevski <alekmitrevski@hotmail.com>
2017-04-02 00:59:58 -0700

    added loader motor conotrol to joystick

--- robot.py
+++ robot.py
@@ -9,51 +9,77 @@ from networktables import NetworkTables
 
 
 class SteampedeRobot(wpilib.IterativeRobot):
-    '''robot code for steampede'''
+    ''' robot code for steampede '''
+
+    def __init__(self):
+        super().__init__()
+
+        self.smart_dashboard = None
+        self.shooter_speed = 1.0
+        self.shooter_enabled = False
+        self.loader_enabled = False
+        self.gear_arm_up = False
+        self.drive_rf_motor = None
+        self.drive_rr_motor = None
+        self.drive_lf_motor = None
+        self.drive_lr_motor = None
+        self.shooter_motor = None
+        self.gear_arm_motor = None
+        self.loader_motor = None
+        self.left_stick = None
+        self.right_stick = None
+        self.drive = None
 
     def robotInit(self):
-        '''Robot initilization function'''
-        '''initialize networktables'''
-        self.sd = NetworkTables.getTable("SmartDashboard")
+        ''' Robot initilization function '''
 
-        self.shooter_speed = 1.0
-        self.sd.putNumber('shooter_speed', self.shooter_speed)
+        # initialize networktables
+        self.smart_dashboard = NetworkTables.getTable("SmartDashboard")
+        self.smart_dashboard.putNumber('shooter_speed', self.shooter_speed)
 
-        '''initialize the camera'''
+        # initialize and launch the camera
         wpilib.CameraServer.launch()
 
-        '''object that handles basic drive operatives'''
+        # object that handles basic drive operatives
         self.drive_rf_motor = wpilib.Victor(6)
         self.drive_rr_motor = wpilib.Victor(7)
         self.drive_lf_motor = wpilib.Victor(8)
         self.drive_lr_motor = wpilib.Victor(9)
         self.shooter_motor = wpilib.Victor(0)
         self.gear_arm_motor = wpilib.Spark(1)
-        self.loader_motor= wpilib.Spark(2)
+        self.loader_motor = wpilib.Spark(2)
 
-        self.myRobot = wpilib.RobotDrive(self.drive_lf_motor, self.drive_lr_motor, self.drive_rf_motor, self.drive_rr_motor)
-        self.myRobot.setExpiration(0.1)
+        self.drive = wpilib.RobotDrive(self.drive_lf_motor, self.drive_lr_motor,
+                                       self.drive_rf_motor, self.drive_rr_motor)
 
-        '''joysticks 1 & 2 on the driver station'''
-        self.leftStick = wpilib.Joystick(0)
-        self.rightStick = wpilib.Joystick(1)
+        self.drive.setExpiration(0.1)
 
-        self.shooter_enabled = False
+        # joysticks 1 & 2 on the driver station
+        self.left_stick = wpilib.Joystick(0)
+        self.right_stick = wpilib.Joystick(1)
 
     def teleopInit(self):
         '''Executed at the start of teleop mode'''
-        self.myRobot.setSafetyEnabled(True)
+        self.drive.setSafetyEnabled(True)
 
     def teleopPeriodic(self):
         '''Runs the motor with tank steering'''
-        self.myRobot.tankDrive(self.leftStick, self.rightStick, True)
+        self.drive.tankDrive(self.left_stick, self.right_stick, True)
 
-        if self.leftStick.getTrigger():
+        if self.left_stick.getTrigger():
             self.shooter_enabled = True
-            self.shooter_motor.set(self.leftStick.getAxis(wpilib.Joystick.AxisType.kZ))
+            self.shooter_motor.set(self.left_stick.getAxis(wpilib.Joystick.AxisType.kZ))
         else:
             self.shooter_enabled = False
             self.shooter_motor.set(0)
+
+        if self.right_stick.getTrigger():
+            self.loader_enabled = True
+            self.loader_motor.set(self.right_stick.getAxis(wpilib.Joystick.AxisType.kZ))
+        else:
+            self.loader_enabled = False
+            self.loader_motor.set(0)
+
 
 if __name__ == '__main__':
     wpilib.run(SteampedeRobot)
