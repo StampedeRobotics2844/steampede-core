f83af64b19c3148064f1d9da6f89123d37530e7e
Jacob Miller <JacobMiller1201@gmail.com>
2017-04-04 23:33:31 -0700

    bug fixes added port map

--- robot.py
+++ robot.py
@@ -6,6 +6,7 @@ Team 2844 @2017
 '''
 import wpilib
 import logging
+import robotmap
 from networktables import NetworkTables
 from robotpy_ext.autonomous import AutonomousModeSelector
 
@@ -19,12 +20,17 @@ class SteampedeRobot(wpilib.IterativeRobot):
 
         self.smart_dashboard = None
         self.shooter_speed = 1.0
-        self.gear_arm_speed = .25
-        self.gear_arm_duration = 1
+
+        self.gear_arm_opening_speed = -1.0
+        self.gear_arm_closing_speed = 1.0
+
+        self.loader_speed = 1.0
+
         self.shooter_enabled = False
         self.loader_enabled = False
         
         self.gear_arm_opened = False
+        self.gear_arm_closed = True
         self.gear_arm_opening = False
         self.gear_arm_closing = False
 
@@ -48,18 +54,21 @@ class SteampedeRobot(wpilib.IterativeRobot):
         # initialize networktables
         self.smart_dashboard = NetworkTables.getTable("SmartDashboard")
         self.smart_dashboard.putNumber('shooter_speed', self.shooter_speed)
+        self.smart_dashboard.putNumber('gear_arm_opening_speed', self.gear_arm_opening_speed)
+        self.smart_dashboard.putNumber('gear_arm_closing_speed', self.gear_arm_closing_speed)
+        self.smart_dashboard.putNumber('loader_speed', self.loader_speed)
 
         # initialize and launch the camera
         wpilib.CameraServer.launch()
 
         # object that handles basic drive operatives
-        self.drive_rf_motor = wpilib.Victor(6)
-        self.drive_rr_motor = wpilib.Victor(7)
-        self.drive_lf_motor = wpilib.Victor(8)
-        self.drive_lr_motor = wpilib.Victor(9)
-        self.shooter_motor = wpilib.Victor(0)
-        self.gear_arm_motor = wpilib.Spark(1)
-        self.loader_motor = wpilib.Spark(2)
+        self.drive_rf_motor = wpilib.Victor(robotmap.motors.right_front)
+        self.drive_rr_motor = wpilib.Victor(robotmap.motors.right_rear)
+        self.drive_lf_motor = wpilib.Victor(robotmap.motors.left_front)
+        self.drive_lr_motor = wpilib.Victor(robotmap.motors.left_rear)
+        self.shooter_motor = wpilib.Victor(robotmap.motors.shooter)
+        self.gear_arm_motor = wpilib.Spark(robotmap.motors.gear_arm)
+        self.loader_motor = wpilib.Spark(robotmap.motors.loader)
 
         # initialize drive
         self.drive = wpilib.RobotDrive(self.drive_lf_motor, self.drive_lr_motor,
@@ -81,8 +90,6 @@ class SteampedeRobot(wpilib.IterativeRobot):
 
         self.automodes = AutonomousModeSelector('autonomous', self.components)
         
-        self.gear_timer = wpilib.Timer()        
-
     def teleopInit(self):
         '''Executed at the start of teleop mode'''
         self.drive.setSafetyEnabled(True)
@@ -93,45 +100,39 @@ class SteampedeRobot(wpilib.IterativeRobot):
 
         if self.left_stick.getTrigger():
             self.shooter_enabled = True
-            self.shooter_motor.set(self.left_stick.getAxis(wpilib.Joystick.AxisType.kZ))
+            axis = self.left_stick.getAxis(wpilib.Joystick.AxisType.kZ)
+            if axis != self.shooter_speed:
+                self.shooter_speed = axis
+                self.smart_dashboard.putNumber('shooter_speed', self.shooter_speed)
+            self.shooter_motor.set(self.shooter_speed)
         else:
             self.shooter_enabled = False
             self.shooter_motor.set(0)
 
         if self.right_stick.getTrigger():
             self.loader_enabled = True
-            self.loader_motor.set(self.right_stick.getAxis(wpilib.Joystick.AxisType.kZ))
+            axis = self.right_stick.getAxis(wpilib.Joystick.AxisType.kZ)
+            if axis != self.loader_speed:
+                self.loader_speed = axis
+                self.smart_dashboard.putNumber('loader_speed', self.loader_speed)
+            self.loader_motor.set(self.loader_speed)
         else:
             self.loader_enabled = False
             self.loader_motor.set(0)
                     
-        if self.right_stick.getRawButton(2) or self.left_stick.getRawButton(2):            
-            if self.gear_arm_opened and self.gear_arm_closing == False:
-                self.gear_timer.reset()
-                self.gear_timer.start()
-                self.gear_arm_motor.set(self.gear_arm_speed)
-                self.gear_arm_closing = True
-        elif self.right_stick.getRawButton(3) or self.left_stick.getRawButton(3):
-            if not self.gear_arm_opened and self.gear_arm_opening == False:
-                self.gear_timer.reset()
-                self.gear_timer.start()
-                self.gear_arm_motor.set(self.gear_arm_speed * -1)
-                self.gear_arm_opening = True
-
-        if self.gear_arm_closing:
-            if self.gear_timer.hasPeriodPassed(self.gear_arm_duration):
-                self.gear_arm_opened = False
-                self.gear_arm_closing = False
-                self.gear_arm_opening = False
-                self.gear_arm_motor.set(0)
-                self.gear_timer.stop()
-        elif self.gear_arm_opening:
-            if self.gear_timer.hasPeriodPassed(self.gear_arm_duration):                
-                self.gear_arm_opened = True
-                self.gear_arm_opening = False
-                self.gear_arm_closing = False
-                self.gear_arm_motor.set(0)
-                self.gear_timer.stop()
+        if self.right_stick.getRawButton(2) or self.left_stick.getRawButton(2):    
+            self.gear_arm_closing = True        
+            self.gear_arm_motor.set(self.gear_arm_closing_speed)
+        else:
+            self.gear_arm_motor.set(0)
+            self.gear_arm_closing = False
+        
+        if self.right_stick.getRawButton(3) or self.left_stick.getRawButton(3):
+            self.gear_arm_opening = True
+            self.gear_arm_motor.set(self.gear_arm_opening_speed)
+        else:
+            self.gear_arm_motor.set(0)
+            self.gear_arm_opening = False
 
     def autonomousInit(self):
         self.drive.setSafetyEnabled(True)
